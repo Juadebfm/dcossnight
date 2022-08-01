025d33fbd7250ba341080ba07b631764788ed8d5
Juadebfm <97069600+Juadebfm@users.noreply.github.com>
2022-08-01 08:23:25 -0500

    Update hover.js

--- hover/hover.js
+++ hover/hover.js
@@ -1,7 +1,21 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c','#8e44ad','#3498db','#e67e22','#2ecc71']
-const SQUARES = 500
+const squares = 500
 
-for(let i = 0; i < SQUARES; i++) {
+for(let i = 0; i < squares; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild('square')
+}
+
+function setColor(element) {
     
+}
+
+function removeColor(element) {
+
 }
