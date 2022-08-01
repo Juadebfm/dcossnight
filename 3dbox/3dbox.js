b55cb51dec872fb5fd00395ac52d05d6982e1139
Juadebfm <97069600+Juadebfm@users.noreply.github.com>
2022-08-01 07:39:47 -0500

    Update 3dbox.js

---
+++ 3dbox/3dbox.js
@@ -0,0 +1,13 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            
+        }
+
+    }
+}
+
+createBoxes()
