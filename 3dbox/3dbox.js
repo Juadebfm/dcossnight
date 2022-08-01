458d799031853245e40631671b042f1a2d0f7d4e
Juadebfm <97069600+Juadebfm@users.noreply.github.com>
2022-08-01 07:41:41 -0500

    Update 3dbox.js

--- 3dbox/3dbox.js
+++ 3dbox/3dbox.js
@@ -4,9 +4,10 @@ const btn = document.getElementById('btn')
 function createBoxes() {
     for(let i = 0; i < 4; i++) {
         for(let j = 0; j < 4; j++) {
-            
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = ``
         }
-
     }
 }
 
